694a4e56cc0c9cca2bdd762e2d6007c4d0ceb54a
goldengrape <github@goldengrape.org>
2023-03-15 15:57:28 -0700

    居然就可用了，但中文字符解析错误，还要再改改

--- repo_to_pdf.py
+++ repo_to_pdf.py
@@ -4,12 +4,12 @@ import shutil
 from git import Repo
 import pdfkit
 import streamlit as st
-import platform 
+import platform
 import errno
 import stat
 import subprocess
-
-
+import markdown
+from bs4 import BeautifulSoup
 
 def handle_remove_readonly(func, path, exc):
     excvalue = exc[1]
@@ -18,6 +18,20 @@ def handle_remove_readonly(func, path, exc):
         func(path)
     else:
         raise
+
+def convert_to_html(file_path):
+    ext = os.path.splitext(file_path)[1]
+    with open(file_path, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    if ext in (".md", ".markdown"):
+        return markdown.markdown(content)
+    elif ext in (".txt", ".py"):
+        return f"<pre>{content}</pre>"
+    elif ext == ".html":
+        return content
+    else:
+        return None
 
 st.title("Git Repo to PDF Converter")
 
@@ -34,14 +48,19 @@ if convert_button and repo_url:
         st.write("Repository cloned successfully.")
         st.write("Converting repository content to a single PDF...")
 
-        file_list = []
+        html_files = []
         for root, _, files in os.walk(temp_dir):
             for file in files:
                 file_path = os.path.join(root, file)
                 if file.endswith(".txt") or file.endswith(".md") or file.endswith(".py") or file.endswith(".html"):
-                    file_list.append(file_path)
+                    html_content = convert_to_html(file_path)
+                    if html_content:
+                        html_file = os.path.splitext(file_path)[0] + ".html"
+                        with open(html_file, "w", encoding="utf-8") as f:
+                            f.write(html_content)
+                        html_files.append(html_file)
 
-        if file_list:
+        if html_files:
             if platform.system() == "Windows":
                 wkhtmltopdf_path=r"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
             else:
@@ -51,14 +70,13 @@ if convert_button and repo_url:
 
             config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
 
-    
             pdf_options = {
                 "quiet": "",
                 "enable-local-file-access": None
             }
 
-            st.write(f"Converting the following files: {file_list}")
-            pdfkit.from_file(file_list, "output.pdf", options=pdf_options, configuration=config)
+            st.write(f"Converting the following files: {html_files}")
+            pdfkit.from_file(html_files, "output.pdf", options=pdf_options, configuration=config)
             st.write("PDF generated successfully.")
 
             with open("output.pdf", "rb") as pdf_file:
@@ -69,5 +87,3 @@ if convert_button and repo_url:
     except Exception as e:
         st.error(f"Error: {e}")
 
-    finally:
-        shutil.rmtree(temp_dir, onerror=handle_remove_readonly)
