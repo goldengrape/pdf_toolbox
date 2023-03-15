50322b3b93a66eec2b2d8768448e178edd144532
goldengrape <github@goldengrape.org>
2023-03-15 16:45:35 -0700

    紧凑了一下，但是出现错误，有一个repo中是txt的文件，在生成后报同名html文件不存在

--- repo_to_pdf.py
+++ repo_to_pdf.py
@@ -1,24 +1,13 @@
 import os
 import tempfile
-import shutil
 from git import Repo
 import pdfkit
 import streamlit as st
 import platform
-import errno
-import stat
 import subprocess
 import markdown
-from bs4 import BeautifulSoup
-from nbconvert import HTMLExporter  # 导入HTMLExporter
+from nbconvert import HTMLExporter
 
-def handle_remove_readonly(func, path, exc):
-    excvalue = exc[1]
-    if func in (os.rmdir, os.unlink) and excvalue.errno == errno.EACCES:
-        os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-        func(path)
-    else:
-        raise
 
 def convert_to_html(file_path):
     ext = os.path.splitext(file_path)[1]
@@ -31,13 +20,57 @@ def convert_to_html(file_path):
         return f"<pre>{content}</pre>"
     elif ext == ".html":
         return content
-    elif ext == ".ipynb":  # 添加对.ipynb文件的处理
+    elif ext == ".ipynb":
         html_exporter = HTMLExporter()
         html_exporter.template_name = "lab"
         html_data, _ = html_exporter.from_filename(file_path)
         return html_data
     else:
         return None
+
+
+def clone_repository(repo_url, temp_dir):
+    Repo.clone_from(repo_url, temp_dir)
+    st.write("Repository cloned successfully.")
+
+
+def get_wkhtmltopdf_path():
+    if platform.system() == "Windows":
+        return r"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
+    else:
+        path = subprocess.run(['which', 'wkhtmltopdf'], stdout=subprocess.PIPE)
+        return path.stdout.decode('utf-8').strip()
+
+
+def convert_repo_to_pdf(repo_url):
+    temp_dir = tempfile.mkdtemp()
+    clone_repository(repo_url, temp_dir)
+
+    st.write("Converting repository content to a single PDF...")
+
+    html_files = [os.path.join(root, file) for root, _, files in os.walk(temp_dir) for file in files if file.endswith((".txt", ".md", ".py", ".html", ".ipynb"))]
+
+    html_files = [os.path.splitext(file_path)[0] + ".html" for file_path in html_files if (html_content := convert_to_html(file_path))]
+
+    for html_file, html_content in zip(html_files, [convert_to_html(file_path) for file_path in html_files]):
+        with open(html_file, "w", encoding="utf-8") as f:
+            f.write(html_content)
+
+    if all(os.path.isfile(html_file) for html_file in html_files):
+        pdf_options = {
+            "quiet": "",
+            "enable-local-file-access": None,
+            "encoding": "UTF-8",
+        }
+
+        pdfkit.from_file(html_files, "output.pdf", options=pdf_options, configuration=pdfkit.configuration(wkhtmltopdf=get_wkhtmltopdf_path()))
+
+        st.write("PDF generated successfully.")
+        with open("output.pdf", "rb") as pdf_file:
+            st.download_button("Download PDF", pdf_file.read(), "output.pdf", "application/pdf")
+    else:
+        st.error("Error: Some HTML files could not be created.")
+
 
 st.title("Git Repo to PDF Converter")
 
@@ -46,51 +79,7 @@ convert_button = st.button("Convert to PDF")
 
 if convert_button and repo_url:
     st.write("Cloning the repository...")
-
-    temp_dir = tempfile.mkdtemp()
     try:
-        Repo.clone_from(repo_url, temp_dir)
-
-        st.write("Repository cloned successfully.")
-        st.write("Converting repository content to a single PDF...")
-
-        html_files = []
-        for root, _, files in os.walk(temp_dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                if file.endswith(".txt") or file.endswith(".md") or file.endswith(".py") or file.endswith(".html")  or file.endswith(".ipynb"):
-                    html_content = convert_to_html(file_path)
-                    if html_content:
-                        html_file = os.path.splitext(file_path)[0] + ".html"
-                        with open(html_file, "w", encoding="utf-8") as f:
-                            f.write(html_content)
-                        html_files.append(html_file)
-
-        if html_files:
-            if platform.system() == "Windows":
-                wkhtmltopdf_path=r"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
-            else:
-                # /usr/bin/wkhtmltopdf
-                wkhtmltopdf_path= subprocess.run(['which', 'wkhtmltopdf'], stdout=subprocess.PIPE)
-                wkhtmltopdf_path=wkhtmltopdf_path.stdout.decode('utf-8').strip()
-
-            config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
-
-            pdf_options = {
-                "quiet": "",
-                "enable-local-file-access": None,
-                "encoding":"UTF-8",
-            }
-
-            pdfkit.from_file(html_files, "output.pdf", options=pdf_options, configuration=config)
-
-            st.write("PDF generated successfully.")
-
-            with open("output.pdf", "rb") as pdf_file:
-                st.download_button("Download PDF", pdf_file.read(), "output.pdf", "application/pdf")
-        else:
-            st.error("No text or code files found in the repository.")
-
+        convert_repo_to_pdf(repo_url)
     except Exception as e:
         st.error(f"Error: {e}")
-
