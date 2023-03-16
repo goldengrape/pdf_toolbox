a5fa1d50f1231eeaab6fdd0ea2da102334870e8d
goldengrape <github@goldengrape.org>
2023-03-15 19:25:50 -0700

    重构repo to pdf

--- repo_to_pdf.py
+++ repo_to_pdf.py
@@ -42,23 +42,14 @@ def get_wkhtmltopdf_path():
         return path.stdout.decode('utf-8').strip()
 
 
-def convert_repo_to_pdf(repo_url):
-    temp_dir = tempfile.mkdtemp()
-    clone_repository(repo_url, temp_dir)
-
-    st.write("Converting repository content to a single PDF...")
-
-    html_files = [os.path.join(root, file) for root, _, files in os.walk(temp_dir) for file in files if file.endswith((".txt", ".md", ".py", ".html", ".ipynb"))]
-
-    html_contents = [convert_to_html(file_path) for file_path in html_files]
-
-    html_files = [os.path.splitext(file_path)[0] + ".html" for file_path, html_content in zip(html_files, html_contents) if html_content]
-
+def create_html_files(temp_dir, html_files, html_contents):
     for html_file, html_content in zip(html_files, html_contents):
         if html_content:
             with open(html_file, "w", encoding="utf-8") as f:
                 f.write(html_content)
 
+
+def generate_pdf_from_html_files(html_files):
     pdf_options = {
         "quiet": "",
         "enable-local-file-access": None,
@@ -72,14 +63,36 @@ def convert_repo_to_pdf(repo_url):
         st.download_button("Download PDF", pdf_file.read(), "output.pdf", "application/pdf")
 
 
-st.title("Git Repo to PDF Converter")
+def convert_repo_to_pdf(repo_url):
+    temp_dir = tempfile.mkdtemp()
+    clone_repository(repo_url, temp_dir)
 
-repo_url = st.text_input("Enter the Git Repository URL:")
-convert_button = st.button("Convert to PDF")
+    st.write("Converting repository content to a single PDF...")
 
-if convert_button and repo_url:
-    st.write("Cloning the repository...")
-    try:
-        convert_repo_to_pdf(repo_url)
-    except Exception as e:
-        st.error(f"Error: {e}")
+    html_files = [os.path.join(root, file) for root, _, files in os.walk(temp_dir) for file in files if file.endswith((".txt", ".md", ".py", ".html", ".ipynb"))]
+
+    html_contents = [convert_to_html(file_path) for file_path in html_files]
+
+    html_files = [os.path.splitext(file_path)[0] + ".html" for file_path, html_content in zip(html_files, html_contents) if html_content]
+
+    create_html_files(temp_dir, html_files, html_contents)
+
+    generate_pdf_from_html_files(html_files)
+
+
+def main():
+    st.title("Git Repo to PDF Converter")
+
+    repo_url = st.text_input("Enter the Git Repository URL:")
+    convert_button = st.button("Convert to PDF")
+
+    if convert_button and repo_url:
+        st.write("Cloning the repository...")
+        try:
+            convert_repo_to_pdf(repo_url)
+        except Exception as e:
+            st.error(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
